44a88243582db6c1d3359e15baf8996854141514
Joleena Velez <joleenavelez@gmail.com>
2021-11-12 23:35:09 -0800

    Fixed duplicate function

--- index.js
+++ index.js
@@ -1,3 +1,4 @@
+
 // Function replaces sprite name, source path, and location
 
 function newImage(name, url, left, bottom) {
@@ -7,63 +8,34 @@ function newImage(name, url, left, bottom) {
     name.style.left = left
     name.style.bottom = bottom
     document.body.append(name)
+    return name;
 }
 
 function newItem(name,url,left,bottom) {
-    name = document.createElement('img')
-    name.src = url
-    name.style.position = 'fixed'
-    name.style.left = left
-    name.style.bottom = bottom
-    document.body.append(name)
+   name = newImage(name, url, left, bottom);
 
     name.addEventListener('dblclick', function(){
         name.remove()
     })
 }
 
+/*
 //Unfinished Bonus
-/*function backGround(name, times, url) {
-    for (var i = 0; i < times; i++) {
-    document.body.append(name)
-    
-}
-} 
-
-let grass
-backGround(grass, 15,'assets/grass.png',)
+function backGround(name, url, height, width,) {
+    for (var w = 0; w < width; w++) {
+        for (var h = 0; h < height; h++) {
+        } 
 */
 
 // New Images
-// let allows variable to be accessed from anywhere inside or outside the function
-
-let greenCharacter
-newImage(greenCharacter,'assets/green-character.gif','100px','100px')
-
-let pineTree
-newImage(pineTree,'assets/pine-tree.png','450px','200px')
-
-let tree
-newImage(tree,'assets/tree.png','200px','300px')
-
-let pillar
-newImage(tree,'assets/pillar.png','350px','100px')
-
-let crate
-newImage(crate,'assets/crate.png','150px','200px')
-
-let well
-newImage(well,'assets/well.png','500px','425px')
+newImage('greenCharacter','assets/green-character.gif','100px','100px')
+newImage('pineTree','assets/pine-tree.png','450px','200px')
+newImage('tree','assets/tree.png','200px','300px')
+newImage('pillar','assets/pillar.png','350px','100px')
+newImage('crate','assets/crate.png','150px','200px')
+newImage('well','assets/well.png','500px','425px')
 
 // New Items
-
-let sword
-newItem(sword,'assets/sword.png','500px','405px')
-
-let shield
-newItem(shield,'assets/shield.png','165px','185px')
-
-let staff
-newItem(staff, 'assets/staff.png','600px','100px')
-
-//bg
+newItem('sword','assets/sword.png','500px','405px')
+newItem('shield','assets/shield.png','165px','185px')
+newItem('staff', 'assets/staff.png','600px','100px')
